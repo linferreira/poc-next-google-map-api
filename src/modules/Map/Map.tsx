ce8384be9034b831cc7335ff4dafe07204c3a9b1
Lindsay Ferreira <lindsayferreira@ext.cvccorp.com.br>
2022-10-13 16:26:50 -0300

    feat: create first map version

--- src/modules/Map/Map.tsx
+++ src/modules/Map/Map.tsx
@@ -1,7 +1,61 @@
-import React from 'react'
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import { API_KEY } from "../../constants/enviroments";
+
+type Props = {
+  text: string;
+  lat: number;
+  lng: number;
+};
+
+const AnyReactComponent = ({ text }: Props) => (
+  <div
+    style={{
+      width: 30,
+      height: 30,
+      borderRadius: 30,
+      background: "red",
+      color: "white",
+    }}
+  />
+);
 
 export default function Map() {
+  const [position, setPosition] = React.useState({
+    lat: -14.235004,
+    lng: -51.925282,
+  });
+  const [zoom, setZoom] = React.useState(5);
+
+  React.useEffect(() => {
+    if (typeof navigator !== "undefined") {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+          setPosition({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          });
+          setZoom(11);
+        });
+      }
+    }
+  }, []);
+
   return (
-    <div>Map</div>
-  )
+    // Important! Always set the container height explicitly
+    <div style={{ height: "100vh", width: "100%" }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: API_KEY, libraries: ["places"] }}
+        center={position}
+        defaultZoom={zoom}
+        zoom={zoom}
+      >
+        <AnyReactComponent
+          lat={-23.4660121}
+          lng={-47.4951893}
+          text="My Marker"
+        />
+      </GoogleMapReact>
+    </div>
+  );
 }
