875c41c7878fece65da137069849cfe1eaea24c3
Lindsay Ferreira <lindsayferreira@ext.cvccorp.com.br>
2022-10-17 09:54:56 -0300

    feat: add select to set current brazilian state in the map

---
+++ src/modules/Map/Map.styles.ts
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+
+  .select {
+    position: absolute;
+    margin: 15px 15px;
+    padding: 0;
+    z-index: 10;
+  }
+`;
--- src/modules/Map/Map.tsx
+++ src/modules/Map/Map.tsx
@@ -3,12 +3,24 @@ import GoogleMapReact from "google-map-react";
 import { API_KEY } from "../../constants/enviroments";
 import { theme } from "../../constants/mapStyle";
 import { stores } from "../../constants/stores";
+import { Container } from "./Map.styles";
+import { brazilianStates } from "../../constants/brazilianStates";
+import Select from "../../components/Select/Select";
 
 type Props = {
   text: string;
   lat: number;
   lng: number;
 };
+
+type SelectValue = {
+  value: string;
+  label: string;
+  position: {
+    lat: number;
+    lng: number;
+  }
+}
 
 const AnyReactComponent = (props: Props) => (
   <div
@@ -22,14 +34,19 @@ const AnyReactComponent = (props: Props) => (
   />
 );
 
-
 export default function Map() {
   const [bounds, setBounds] = React.useState(null);
   const [position, setPosition] = React.useState({
     lat: -14.235004,
     lng: -51.925282,
   });
+  const [stateSelected, setStateSelected] = React.useState(null);
   const [zoom, setZoom] = React.useState(5);
+
+  function handleChangeSelect(newValue: SelectValue) {
+    setStateSelected(newValue);
+    setPosition(newValue.position);
+  }
 
   React.useEffect(() => {
     if (typeof navigator !== "undefined") {
@@ -46,7 +63,20 @@ export default function Map() {
   }, []);
 
   return (
-    <div>
+    <Container>
+      <Select
+        className="select"
+        name={"states select"}
+        onChange={handleChangeSelect}
+        value={stateSelected}
+        placeholder="Selecionar estado"
+        noOptionsMessage={() => "Estado não encontrado."}
+        options={brazilianStates || []}
+        menuPortalTarget={
+          typeof window !== "undefined" ? window.document.body : null
+        }
+        menuPosition="fixed"
+      />
       <GoogleMapReact
         bootstrapURLKeys={{ key: API_KEY, libraries: ["places"] }}
         center={position}
@@ -55,9 +85,9 @@ export default function Map() {
         style={{ height: "100vh", width: "100%" }}
         options={{ styles: theme }}
         onChange={({ center }) => {
-          const {lat, lng} = center;
+          const { lat, lng } = center;
           setZoom(zoom);
-          setPosition({ lat, lng });          
+          setPosition({ lat, lng });
         }}
       >
         {stores.map((store) => (
@@ -69,6 +99,6 @@ export default function Map() {
           />
         ))}
       </GoogleMapReact>
-    </div>
+    </Container>
   );
 }
