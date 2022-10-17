eca3ed708cddae346414cbb0eca286fefe3b4244
Lindsay Ferreira <lindsayferreira@ext.cvccorp.com.br>
2022-10-17 09:53:59 -0300

    feat: add brazilian states const

---
+++ src/constants/brazilianStates.ts
@@ -0,0 +1,219 @@
+export const brazilianStates = [
+  {
+    position: {
+      lat: -9.974,
+      lng: -67.8076,
+    },
+    label: "Acre",
+    value: "AC",
+  },
+  {
+    position: {
+      lat: -9.66625,
+      lng: -35.717239,
+    },
+    label: "Alagoas",
+    value: "AL",
+  },
+  {
+    position: {
+      lat: 0.0344566,
+      lng: -51.0666,
+    },
+    label: "Amapá",
+    value: "AP",
+  },
+  {
+    position: {
+      lat: -3.10719,
+      lng: -60.0261,
+    },
+    label: "Amazonas",
+    value: "AM",
+  },
+  {
+    position: {
+      lat: -12.9704,
+      lng: -38.5124,
+    },
+    label: "Bahia",
+    value: "BA",
+  },
+  {
+    position: {
+      lat: -3.731862,
+      lng: -38.526669,
+    },
+    label: "Ceará",
+    value: "CE",
+  },
+  {
+    position: {
+      lat: -15.7801,
+      lng: -47.9292,
+    },
+    label: "Distrito Federal",
+    value: "DF",
+  },
+  {
+    position: {
+      lat: -20.3222,
+      lng: -40.3381,
+    },
+    label: "Espírito Santo",
+    value: "ES",
+  },
+  {
+    position: {
+      lat: -16.6799,
+      lng: -49.255,
+    },
+    label: "Goiás",
+    value: "GO",
+  },
+  {
+    position: {
+      lat: -2.53073,
+      lng: -44.3068,
+    },
+    label: "Maranhão",
+    value: "MA",
+  },
+  {
+    position: {
+      lat: -15.5989,
+      lng: -56.0949,
+    },
+    label: "Mato Grosso",
+    value: "MT",
+  },
+  {
+    position: {
+      lat: -20.4435,
+      lng: -54.6478,
+    },
+    label: "Mato Grosso do Sul",
+    value: "MS",
+  },
+  {
+    position: {
+      lat: -19.8157,
+      lng: -43.9542,
+    },
+    label: "Minas Gerais",
+    value: "MG",
+  },
+  {
+    position: {
+      lat: -1.45502,
+      lng: -48.502,
+    },
+    label: "Pará",
+    value: "PA",
+  },
+  {
+    position: {
+      lat: -7.11532,
+      lng: -34.861,
+    },
+    label: "Paraíba",
+    value: "PB",
+  },
+  {
+    position: {
+      lat: -25.4284,
+      lng: -49.2733,
+    },
+    label: "Paraná",
+    value: "PR",
+  },
+  {
+    position: {
+      lat: -8.05428,
+      lng: -34.8813,
+    },
+    label: "Pernambuco",
+    value: "PE",
+  },
+  {
+    position: {
+      lat: -5.08921,
+      lng: -42.8016,
+    },
+    label: "Piauí",
+    value: "PI",
+  },
+  {
+    position: {
+      lat: -22.9035,
+      lng: -43.2096,
+    },
+    label: "Rio de Janeiro",
+    value: "RJ",
+  },
+  {
+    position: {
+      lat: -5.79448,
+      lng: -35.211,
+    },
+    label: "Rio Grande do Norte",
+    value: "RN",
+  },
+  {
+    position: {
+      lat: -32.0332,
+      lng: -52.0986,
+    },
+    label: "Rio Grande do Sul",
+    value: "RS",
+  },
+  {
+    position: {
+      lat: -8.76183,
+      lng: -63.902,
+    },
+    label: "Rondônia",
+    value: "RO",
+  },
+  {
+    position: {
+      lat: 2.81954,
+      lng: -60.6714,
+    },
+    label: "Roraima",
+    value: "RR",
+  },
+  {
+    position: {
+      lat: -27.5969,
+      lng: -48.5495,
+    },
+    label: "Santa Catarina",
+    value: "SC",
+  },
+  {
+    position: {
+      lat: -23.5489,
+      lng: -46.6388,
+    },
+    label: "São Paulo",
+    value: "SP",
+  },
+  {
+    position: {
+      lat: -10.9095,
+      lng: -37.0748,
+    },
+    label: "Sergipe",
+    value: "SE",
+  },
+  {
+    position: {
+      lat: -10.1689,
+      lng: -48.3317,
+    },
+    label: "Tocantins",
+    value: "TO",
+  },
+];
+
