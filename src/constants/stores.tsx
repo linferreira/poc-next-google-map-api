80c21b85b46dccfdeaee2ae4ce11220e1c30d9be
Lindsay Ferreira <lindsayferreira@ext.cvccorp.com.br>
2022-10-17 12:45:09 -0300

    feat: add new stores

--- src/constants/stores.tsx
+++ src/constants/stores.tsx
@@ -1,87 +1,418 @@
 export interface StoresProps {
-  id: number;
   name: string;
-  address: string;
   position: {
     lat: number;
     lng: number;
   };
-  tel: number;
 }
 
 export const stores: StoresProps[] = [
   {
-    id: 1,
     name: "CVC Loja Anápolis - Goiás",
-    address:
-      "Lojas 01 e 02 - Avenida Goiás, 30, CEP:, 75113-150 - Quadra 05 Lote 30, Anápolis - GO, 75113-150",
-    tel: 99999999,
     position: {
       lat: -16.32882674261577,
       lng: -48.94809187327514,
     },
   },
   {
-    id: 2,
     name: "CVC Brasil Park Shopping",
-    address:
-      "Av. Brasil Norte, 505 - Vila das Acacias, Anápolis - GO, 75113-570",
-    tel: 99999999,
     position: {
       lat: -16.32421030696828,
       lng: -48.9490782982139,
     },
   },
   {
-    id: 3,
     name: "CVC",
-    address:
-      "R. Quinze de Dezembro, 135 - St. Central, Anápolis - GO, 75024-070",
-    tel: 99999999,
     position: {
       lat: -16.326376648249088,
       lng: -48.95566680122565,
     },
   },
   {
-    id: 4,
     name: "Operadora e Agência de Viagens Cvc Tur",
-    address:
-      "R. Quinze de Dezembro, 135 - St. Central, Anápolis - GO, 75024-070",
-    tel: 99999999,
     position: {
       lat: -16.328188772126303,
       lng: -48.95721175349883,
     },
   },
   {
-    "id": 5,
-    "name": "CVC Iguatemi Esplanada Shopping",
-    "address": "Avenida Professora Izoraida Marques Peres, 401 - Box 315 - Parque Campolim, Sorocaba - SP, 18047-900",
-    "tel": 99999999,
-    "position": {
-      "lat": -23.537630,
-      "lng": -47.466170
-    }
+    name: "CVC Iguatemi Esplanada Shopping",
+    position: {
+      lat: -23.53763,
+      lng: -47.46617,
+    },
   },
   {
-    "id": 6,
-    "name": "CVC Shopping Cidade",
-    "address": "Av. Itavuvu, 3365 - 2024/2025 - Jardim Santa Cecilia, Sorocaba - SP, 18078-005",
-    "tel": 99999999,
-    "position": {
-      "lat": -23.456060,
-      "lng": -47.481910
-    }
+    name: "CVC Shopping Cidade",
+    position: {
+      lat: -23.45606,
+      lng: -47.48191,
+    },
   },
   {
-    "id": 7,
-    "name": "CVC Pátio Cianê",
-    "address": "Av. Dr. Afonso Vergueiro, 823 - loja 20408 - Centro, Sorocaba - SP, 18035-370",
-    "tel": 99999999,
-    "position": {
-      "lat": -23.520070,
-      "lng": -46.585470
-    }
-  }
+    name: "CVC Pátio Cianê",
+    position: {
+      lat: -23.52007,
+      lng: -46.58547,
+    },
+  },
+  {
+    name: "CVC Turismo Shopping Ponta Negra",
+    position: {
+      lat: -3.0890952,
+      lng: -60.0635306,
+    },
+  },
+  {
+    name: "CVC Turismo Shopping Paralela",
+    position: {
+      lat: -12.9261199,
+      lng: -38.38022,
+    },
+  },
+  {
+    name: "CVC Turismo Tricenter",
+    position: {
+      lat: -3.7349533,
+      lng: -38.5662365,
+    },
+  },
+  {
+    name: "North Shopping Fortaleza",
+    position: {
+      lat: -3.735406,
+      lng: -38.566154,
+    },
+  },
+  {
+    name: "North Shopping Jóquei",
+    position: {
+      lat: -3.764912,
+      lng: -38.574638,
+    },
+  },
+  {
+    name: "Shopping Iguatemi Fortaleza",
+    position: {
+      lat: -3.7349533,
+      lng: -38.5662365,
+    },
+  },
+  {
+    name: "Alnameda Shopping - Taguatinga",
+    position: {
+      lat: -15.836683,
+      lng: -48.056816,
+    },
+  },
+  {
+    name: "CVC Conjunto Nacional",
+    position: {
+      lat: -15.791572,
+      lng: -47.883224,
+    },
+  },
+  {
+    name: "CVC Shopping Mestre Álvaro",
+    position: {
+      lat: -20.240738,
+      lng: -40.274872,
+    },
+  },
+  {
+    name: "CVC Shopping Campo Grande",
+    position: {
+      lat: -20.457836,
+      lng: -54.587376,
+    },
+  },
+  {
+    name: "CVC Center Shopping Uberlândia",
+    position: {
+      lat: -18.909534,
+      lng: -48.260643,
+    },
+  },
+  {
+    name: "CVC Mister Shopping",
+    position: {
+      lat: -21.759501,
+      lng: -43.348167,
+    },
+  },
+  {
+    name: "CVC Shopping Tambiá",
+    position: {
+      lat: -7.115963,
+      lng: -34.875877,
+    },
+  },
+  {
+    name: "CVC Shopping São José",
+    position: {
+      lat: -25.537951,
+      lng: -49.204189,
+    },
+  },
+  {
+    name: "CVC Shopping Total Curitiba",
+    position: {
+      lat: -25.478477,
+      lng: -49.295387,
+    },
+  },
+  {
+    name: "CVC River Shopping",
+    position: {
+      lat: -9.393919,
+      lng: -40.493462,
+    },
+  },
+  {
+    name: "CVC Shopping Center Guararapes",
+    position: {
+      lat: -8.167229,
+      lng: -34.919334,
+    },
+  },
+  {
+    name: "CVC Shopping Tacaruna",
+    position: {
+      lat: -8.038111,
+      lng: -34.871422,
+    },
+  },
+
+  {
+    name: "CVC Shopping Midway Mall",
+    position: {
+      lat: -5.812095,
+      lng: -35.206814,
+    },
+  },
+  {
+    name: "CVC Barra Shopping",
+    position: {
+      lat: -22.99745,
+      lng: -43.359089,
+    },
+  },
+  {
+    name: "CVC Boulevard Rio Shopping",
+    position: {
+      lat: -22.919088,
+      lng: -43.250492,
+    },
+  },
+  {
+    name: "CVC Boulevard Shopping São Gonçalo",
+    position: {
+      lat: -22.822271,
+      lng: -43.046364,
+    },
+  },
+  {
+    name: "CVC 	Cadima Shopping",
+    position: {
+      lat: -22.289413,
+      lng: -42.531895,
+    },
+  },
+  {
+    name: "CVC Partage Shopping São Gonçalo",
+    position: {
+      lat: -22.821779,
+      lng: -43.045891,
+    },
+  },
+  {
+    name: "CVC Rio Design Centre Barra da Tijuca",
+    position: {
+      lat: -23.001301,
+      lng: -43.385612,
+    },
+  },
+
+  {
+    name: "CVC Shopping Icaraí",
+    position: {
+      lat: -22.90642,
+      lng: -43.111187,
+    },
+  },
+  {
+    name: "CVC Shopping Nova América",
+    position: {
+      lat: 22.878687,
+      lng: -43.271027,
+    },
+  },
+  {
+    name: "CVC Lindóia Shopping",
+    position: {
+      lat: -30.010042,
+      lng: -51.152302,
+    },
+  },
+  {
+    name: "CVC Moinhos Shopping",
+    position: {
+      lat: -30.02355,
+      lng: -51.201267,
+    },
+  },
+  {
+    name: "CVC 	Royal Plaza Shopping Santa Maria",
+    position: {
+      lat: -29.689564,
+      lng: -53.795437,
+    },
+  },
+  {
+    name: "CVC Shopping Gravataí",
+    position: {
+      lat: -29.950603,
+      lng: -50.996044,
+    },
+  },
+  {
+    name: "CVC Shopping Total Porto Alegre",
+    position: {
+      lat: -30.025705,
+      lng: -51.212402,
+    },
+  },
+  {
+    name: "CVC Balneário Camboriú Shopping",
+    position: {
+      lat: -26.992785,
+      lng: -48.645264,
+    },
+  },
+  {
+    name: "CVC Farol Shopping Tubarão",
+    position: {
+      lat: -28.480509,
+      lng: -49.002815,
+    },
+  },
+  {
+    name: "CVC Shopping Neumarkt Blumenau",
+    position: {
+      lat: -26.920826,
+      lng: -49.06992,
+    },
+  },
+  {
+    name: "CVC 	Shopping Pátio Chapecó",
+    position: {
+      lat: -27.070169,
+      lng: -52.626873,
+    },
+  },
+  {
+    name: "CVC Araçatuba Shopping",
+    position: {
+      lat: -21.220856,
+      lng: -50.431404,
+    },
+  },
+  {
+    name: "CVC Bebedouro Shopping",
+    position: {
+      lat: -20.949877,
+      lng: -48.493504,
+    },
+  },
+  {
+    name: "CVC Boavista Shopping ",
+    position: {
+      lat: -23.6551,
+      lng: -46.700798,
+    },
+  },
+  {
+    name: "CVC Buriti Shopping Guará",
+    position: {
+      lat: -22.811279,
+      lng: -45.198696,
+    },
+  },
+  {
+    name: "CVC Centervale Shopping",
+    position: {
+      lat: -23.200172,
+      lng: -45.881695,
+    },
+  },
+  {
+    name: "CVC Galleria Shopping",
+    position: {
+      lat: -22.862829,
+      lng: -47.022964,
+    },
+  },
+  {
+    name: "CVC Jardim Sul Shopping",
+    position: {
+      lat: -23.631207,
+      lng: -46.736023,
+    },
+  },
+  {
+    name: "CVC Litoral Plaza Shopping",
+    position: {
+      lat: -23.997486,
+      lng: -46.40844,
+    },
+  },
+  {
+    name: "CVC Mais Shopping",
+    position: {
+      lat: -23.652966,
+      lng: -46.710541,
+    },
+  },
+  {
+    name: "CVC 	Maxi Shopping Jundiaí",
+    position: {
+      lat: -23.202335,
+      lng: -46.86198,
+    },
+  },
+  {
+    name: "CVC 	Park Shopping São Caetano",
+    position: {
+      lat: -23.626263,
+      lng: -46.580421,
+    },
+  },
+  {
+    name: "CVC 	Paulínia Shopping",
+    position: {
+      lat: -22.782848,
+      lng: -47.153706,
+    },
+  },
+  {
+    name: "CVC 	Prudenshopping",
+    position: {
+      lat: -22.117151,
+      lng: -51.406082,
+    },
+  },
+  {
+    name: "CVC Tivoli Shopping",
+    position: {
+      lat: -22.748877,
+      lng: -47.367985,
+    },
+  },
+  {
+    name: "CVC	Vale Sul Shopping",
+    position: {
+      lat: -23.216501,
+      lng: -45.892681,
+    },
+  },
 ];
